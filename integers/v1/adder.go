7c7e64c0e12a1edad3e16045b6fc8311c3e2193f
tonbiattack <teni213638395636@gmail.com>
2026-02-15 23:54:48 +0900

    fix: Add関数の戻り値を引数の合計に修正

--- integers/v1/adder.go
+++ integers/v1/adder.go
@@ -4,5 +4,6 @@ package integers
 // Add takes two int parameters and returns one int result.
 // `x, y int` is Go's grouped parameter type syntax.
 func Add(x, y int) int {
-	return 4
+	// Arithmetic `+` for ints.
+	return x + y
 }
